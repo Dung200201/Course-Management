db6dd353fc0cf425369602fcda90fcc9e6366c8f
Phan Tien Dung <tiendungphan2k1@gmail.com>
2024-02-28 16:12:25 +0700

    Course-Management

---
+++ src/app/models/Course.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const slug = require('mongoose-slug-updater');
+var mongooseDelete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const Schema = mongoose.Schema;
+
+const Course = new Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String, required: true},
+    level: { type: String },
+    slug: { type: String, slug: 'name', unique: true },
+  }, {
+    timestamps: true,
+  });
+
+  //Add plugin
+mongoose.plugin(slug);
+
+
+Course.plugin(mongooseDelete, { 
+  deletedAt: true,
+  overrideMethods: 'all', 
+});
+
+module.exports = mongoose.model('Course', Course);
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+
+const newRouter = require('./news');
+const meRouter = require('./me');
+const coursesRouter = require('./courses');
+const siteRouter = require('./site');
+
+function route(app) {
+    app.use('/news', newRouter);
+    app.use('/courses', coursesRouter);
+    app.use('/me', meRouter);
+
+    app.use('/', siteRouter);
+}
+
+module.exports = route;
